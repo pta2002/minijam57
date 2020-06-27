acf6c16a70d08bd2b883150499655902e3ab9991
Pedro Alves <pta2002@pta2002.com>
2020-06-27 17:37:18 +0100

    add poles and seats

--- game/passenger.lua
+++ game/passenger.lua
@@ -5,6 +5,7 @@ function Passenger:new(x, y, world)
   self.world = world
 
   self.body = self.world:newCircleCollider(x, y, 10)
+  self.body:setLinearDamping(0.2)
 end
 
 return Passenger
--- game/train.lua
+++ game/train.lua
@@ -16,10 +16,18 @@ function Train:new()
   -------------
   self.world = wf.newWorld(0, 0, true)
 
+  self.seats = { -- {side, x, dir}
+    {0, 3, -1}, {0, 2, -1}, {0, 4, 1}, {0, 6, -1}, {1, 4, -1}
+  }
+
+  self.poles = {
+    {2, 3}, {2, 9}
+  }
+
   self:setWalls()
 
   self.passengers = {}
-  for i = 0, 40 do
+  for i = 0, 10 do
     table.insert(self.passengers, Passenger(love.math.random(self.x + 20, self.x + self.cart_width - 20), love.math.random(self.y + 20, self.y + self.cart_height - 20), self.world))
   end
 end
@@ -33,6 +41,16 @@ function Train:setWalls()
   wall_left:setType('static')
   local wall_right = self.world:newRectangleCollider(self.x + self.cart_width - 5, self.y - 5, 10, self.cart_height + 10)
   wall_right:setType('static')
+
+  for i, pole in ipairs(self.poles) do
+    local pole = self.world:newCircleCollider(self.x + self.cart_width * pole[2]/ 12, self.y + self.cart_height * pole[1] / 4, 2)
+    pole:setType('static')
+  end
+
+  for i, seat in ipairs(self.seats) do
+    local seat = self.world:newRectangleCollider(self.x + seat[2] * self.cart_width / 12 - 5, self.y + (self.cart_height * 2 / 3) * seat[1], 10, self.cart_height / 3)
+    seat:setType('static')
+  end
 end
 
 function Train:update(dt)
@@ -46,7 +64,9 @@ function Train:draw()
   local c1y = love.math.noise(love.timer.getTime() + 100)
   love.graphics.translate(c1x * 5, c1y * 5)
   self:carriageOutline()
+
   love.graphics.setLineWidth(0.5)
+
   self.world:draw()
   love.graphics.pop()
 
